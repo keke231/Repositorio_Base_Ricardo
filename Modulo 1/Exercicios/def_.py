613ba6497b2700cfe946180ce68f1a4e2ff5a87c
keke231 <kethleen23b@gmail.com>
2025-09-17 10:12:01 -0300

    Update and rename Orientações.txt to def_.py

---
+++ Modulo 1/Exercicios/def_.py
@@ -0,0 +1,6 @@
+def acessorios_prata ():
+        print("gosto de usar acessorios prata")
+def acessorios_dourado ():
+        print("nao gosto de usar acessorios dourado")
+
+        opcao = int(input("digite qual voce gosta: \n 1- acessorios prata \n 2- acessorios dourado")) 
