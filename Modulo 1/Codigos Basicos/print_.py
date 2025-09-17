fb86680bac8b020367bdf2c8ed6016fc8e3751d6
keke231 <kethleen23b@gmail.com>
2025-09-17 10:16:43 -0300

    Update and rename Orientações.txt to print_.py

---
+++ Modulo 1/Codigos Basicos/print_.py
@@ -0,0 +1,20 @@
+flamengo = 5
+corinthians = 10
+
+print(corinthians + flamengo)
+print(corinthians - flamengo)
+print(corinthians * flamengo)
+print(corinthians / flamengo)
+print(corinthians % flamengo)
+
+nome = "Kauã" \
+
+print(nome)
+
+print('meu nome e',nome)
+
+idade = 15
+
+print('minha idade e',idade)
+
+print('meu nome e',nome,'minha idade e',idade)
